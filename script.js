56f7f7af65cc645decae37913626fdc154940e0a
Sarah Hollingsworth <sahhollingsworth@gmail.com>
2021-09-22 23:37:21 -0700

    Added logic such that if any character type is selected by the user, the corresponding array's values are added to the pool array. When the password is randomly generated, it will be from the values present in this pool array.
    Add validation logic to alert user at least 1 character type is required if they have selected none.
    
    Acceptance criteria:
    WHEN I answer each prompt
    THEN my input should be validated and at least one character type should be selected

--- script.js
+++ script.js
@@ -10,48 +10,52 @@ var poolLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"
 var poolUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
 // Empty array that will act as bucket for all potential characters for password creation, as determine by user prompts
-var poolGlobal = [];
+var pool = [];
 
-// Function for user to define password length
+// Function to generate random password based on user input
 function generatePassword() {
-  var charsLength = prompt("In the range of 8 to 128 characters, how many characters would you like your password to contain?");
+  // User prompted to choose how many characters are in generated password
+  var passwordLength = prompt("In the range of 8 to 128 characters, how many characters would you like your password to contain?");
   /* This if statement validates that user submission for length is a numerical value. If isNaN returns false, then value is a number. */
-  if (isNaN(charsLength)) {
+  if (isNaN(passwordLength)) {
     alert("Password length must be a number between 8 and 128.");
-  } else if (charsLength >= 8 && charsLength <= 128) {
+  } else if (passwordLength >= 8 && passwordLength <= 128) {
   /* Alerts user if doesn't meet the minimum or maximum character count constraint */
-  } else if (charsLength > 128) {
+  } else if (passwordLength > 128) {
     alert("Password can include a maximum of 128 characters.");
-  } else if (charsLength < 8) {
+  } else if (passwordLength < 8) {
     alert("Password must include at least 8 characters.");
   }
 
 
-// if i add return generatePassword(); to each constraint violation then the user doesn't exit the generation flow, BUT also cannot exit the flow. do i need to add something like below as well? "" is for safari compatibility
-// if (passwordLength === null || passwordLength === "")
-// example:
-// } else if (passwordLength > 128) {
-//   alert("Password can include a maximum of 128 characters.");
-//   return generatePassword();
-// }
-
-
+  // User prompted to choose if they want special characters in generated password
   var charsSpecial = confirm("Click OK to confirm including special characters.");
-  if (charsSpecial=true) {
-    console.log("yeehaw Special characters");
+  if (charsSpecial === true) {
+    pool.push(poolSpecial);
   }
+
+  // User prompted to choose if they want numbers in generated password
   var charsNum = confirm("Click OK to confirm including numeric characters.");
-  if (charsNum=true) {
-    console.log("yeehaw Numeric characters");
+  if (charsNum === true) {
+    pool.push(poolNum);
   }
+
+  // User prompted to choose if they want uppercase characters in generated password
   var charsLower = confirm("Click OK to confirm including lowercase characters.");
-  if (charsLower=true) {
-    console.log("yeehaw Lowercase characters");
+  if (charsLower === true) {
+    pool.push(poolLower);
   } 
+
+  // User prompted to choose if they want uppercase characters in generated password
   var charsUpper = confirm("Click OK to confirm including uppercase characters.");
-  if (charsUpper=true) {
-    console.log("yeehaw Uppercase characters");
+  if (charsUpper === true) {
+    pool.push(poolUpper);
   } 
+
+  //Alerts if user hasn't selected at least 1 character option
+  if (charsSpecial === false && charsNum === false && charsLower === false && charsUpper === false) {
+    alert("You must choose at least one character type to ensure a secure password. Please try again.");
+  }
 }
 
 // Write password to the #password input
